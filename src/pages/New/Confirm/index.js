b4ed62cdf5c9e3eb024f2dbc2ebadb14d8f4d1b7
Enrick <enrickdaltro@gmail.com>
2020-03-17 21:37:10 -0300

    add: confirm page features

--- src/pages/New/Confirm/index.js
+++ src/pages/New/Confirm/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { formatRelative, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Background from '~/components/Background';
+import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Avatar, Name, Time, SubmitButton } from './styles';
 
-export default function Confirm() {
-  return <View />;
+export default function Confirm({ navigation, route }) {
+  const { provider, time } = route.params;
+
+  const dateFormatted = useMemo(
+    () => formatRelative(parseISO(time), new Date(), { locale: pt }),
+    [time],
+  );
+
+  async function handleAddAppointment() {
+    await api.post('appointments', {
+      provider_id: provider.id,
+      date: time,
+    });
+
+    navigation.navigate('dashboard');
+  }
+  return (
+    <Background>
+      <Container>
+        <Avatar
+          source={{
+            uri: provider.avatar
+              ? provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+          }}
+        />
+
+        <Name>{provider.name}</Name>
+
+        <Time>{dateFormatted}</Time>
+
+        <SubmitButton onPress={handleAddAppointment}>
+          Confirmar agendamento
+        </SubmitButton>
+      </Container>
+    </Background>
+  );
 }
